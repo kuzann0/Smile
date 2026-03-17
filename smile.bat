87a6d1cff1c5d9a9ef1610e4723dd64267bb36a8
Kuz <kuzano.1001@gmail.com>
2026-03-17 09:12:00 +0800

    'v1-wip'

---
+++ smile.bat
@@ -0,0 +1,79 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGCxx2oMPBRbQBe+L2azCrBR+uf0+9a0r0QRUa8aYIrYz6CPIuwWpED8cPY=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title Something To Smile About
+prompt -
+cls
+
+:top
+echo.
+echo   what
+color 09
+timeout /t 1 > nul
+cls
+
+echo.
+echo.
+echo         makes
+color 09
+timeout /t 1 > nul
+cls
+
+echo.
+echo.
+echo              genuinely
+color 0c
+
+timeout /t 1 > nul
+cls
+
+
+echo.
+echo.
+echo.
+echo                        smile? 
+color 0E
+timeout /t 1 > nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo                   :)
+color 06
+timeout /t 1 > nul
+cls
+
+goto top
